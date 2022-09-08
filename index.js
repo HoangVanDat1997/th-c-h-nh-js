6960f8041a83bd2ba7d49e0d24cbd724df89c506
hoangvandat <taiphiets666@gmail.com>
2022-09-08 09:01:47 +0700

    thực hành js

---
+++ index.js
@@ -0,0 +1,12 @@
+var inputWidth;
+var inputHeight;
+
+
+inputWidth = promtp("Enter the width")
+inputHeight = promtp("Enter the height")
+
+
+var Width = parseInt(inputWidth)
+var Height =  paraseInt(inputHeight)
+var area = Width * Height ;
+document.write ("the area " + area)
